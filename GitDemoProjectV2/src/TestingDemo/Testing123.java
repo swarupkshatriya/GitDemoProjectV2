b13591e636a1feb70daf35ba6211768b97b91a19
swarupkshatriya <swarup.kshatriya@gmail.com>
2023-04-21 16:45:21 +0530

    adding it to local repository

---
+++ GitDemoProjectV2/src/TestingDemo/Testing123.java
@@ -0,0 +1,10 @@
+package TestingDemo;
+
+public class Testing123 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My name is swarup and I am learning Github");
+	}
+
+}
